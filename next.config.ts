9e68a6211079a3be20a9638bd4fd5759e0677416
mateusfdsm <mateusdesamacedo@gmail.com>
2025-12-20 23:36:50 -0300

    Correção bug vercel

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    devIndicators: {
-      buildActivity: false,
-      buildActivityPosition: 'bottom-right',
-    },
-  },
   /* config options here */
 };
 
